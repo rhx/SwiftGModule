7a6412b2d3ef7b4f27a8cb03bb2e1063b89a7452
Rene Hexel <r.hexel@griffith.edu.au>
2020-03-29 14:10:42 +1000

    regen for gir2swift 8.0.0 and Swift 5.2

--- Sources/GModule/GModule-2.0.swift
+++ Sources/GModule/GModule-2.0.swift
@@ -223,7 +223,7 @@ public extension ModuleRef {
     /// archive) it tries to open the corresponding module. If that fails
     /// and it doesn't have the proper module suffix for the platform
     /// (`G_MODULE_SUFFIX`), this suffix will be appended and the corresponding
-    /// module will be opended. If that fails and `file_name` doesn't have the
+    /// module will be opened. If that fails and `file_name` doesn't have the
     /// ".la"-suffix, this suffix is appended and `g_module_open()` tries to open
     /// the corresponding module. If eventually that fails as well, `nil` is
     /// returned.
@@ -246,15 +246,27 @@ open class Module: ModuleProtocol {
     public let ptr: UnsafeMutableRawPointer
 
     /// Designated initialiser from the underlying `C` data type.
-    /// Ownership is transferred to the `Module` instance.
+    /// This creates an instance without performing an unbalanced retain
+    /// i.e., ownership is transferred to the `Module` instance.
+    /// - Parameter op: pointer to the underlying object
     public init(_ op: UnsafeMutablePointer<GModule>) {
         ptr = UnsafeMutableRawPointer(op)
     }
 
-    /// Reference convenience intialiser for a related type that implements `ModuleProtocol`
+    /// Designated initialiser from the underlying `C` data type.
+    /// `GModule` does not allow reference counting, so despite the name no actual retaining will occur.
+    /// i.e., ownership is transferred to the `Module` instance.
+    /// - Parameter op: pointer to the underlying object
+    public init(retaining op: UnsafeMutablePointer<GModule>) {
+        ptr = UnsafeMutableRawPointer(op)
+        // no reference counting for GModule, cannot ref(cast(_ptr))
+    }
+
+    /// Reference intialiser for a related type that implements `ModuleProtocol`
     /// `GModule` does not allow reference counting.
-    public convenience init<T: ModuleProtocol>(_ other: T) {
-        self.init(cast(other._ptr))
+    /// - Parameter other: an instance of a related type that implements `ModuleProtocol`
+    public init<T: ModuleProtocol>(_ other: T) {
+        ptr = UnsafeMutableRawPointer(other._ptr)
         // no reference counting for GModule, cannot ref(cast(_ptr))
     }
 
@@ -265,26 +277,61 @@ open class Module: ModuleProtocol {
 
     /// Unsafe typed initialiser.
     /// **Do not use unless you know the underlying data type the pointer points to conforms to `ModuleProtocol`.**
-    public convenience init<T>(cPointer: UnsafeMutablePointer<T>) {
-        self.init(cPointer.withMemoryRebound(to: GModule.self, capacity: 1) { $0 })
+    /// - Parameter cPointer: pointer to the underlying object
+    public init<T>(cPointer p: UnsafeMutablePointer<T>) {
+        ptr = UnsafeMutableRawPointer(p)
+    }
+
+    /// Unsafe typed, retaining initialiser.
+    /// **Do not use unless you know the underlying data type the pointer points to conforms to `ModuleProtocol`.**
+    /// - Parameter cPointer: pointer to the underlying object
+    public init<T>(retainingCPointer cPointer: UnsafeMutablePointer<T>) {
+        ptr = UnsafeMutableRawPointer(cPointer)
+        // no reference counting for GModule, cannot ref(cast(_ptr))
     }
 
     /// Unsafe untyped initialiser.
     /// **Do not use unless you know the underlying data type the pointer points to conforms to `ModuleProtocol`.**
-    public convenience init(raw: UnsafeRawPointer) {
-        self.init(UnsafeMutableRawPointer(mutating: raw).assumingMemoryBound(to: GModule.self))
+    /// - Parameter p: raw pointer to the underlying object
+    public init(raw p: UnsafeRawPointer) {
+        ptr = UnsafeMutableRawPointer(mutating: p)
+    }
+
+    /// Unsafe untyped, retaining initialiser.
+    /// **Do not use unless you know the underlying data type the pointer points to conforms to `ModuleProtocol`.**
+    public init(retainingRaw raw: UnsafeRawPointer) {
+        ptr = UnsafeMutableRawPointer(mutating: raw)
+        // no reference counting for GModule, cannot ref(cast(_ptr))
     }
 
     /// Unsafe untyped initialiser.
     /// **Do not use unless you know the underlying data type the pointer points to conforms to `ModuleProtocol`.**
-    public convenience init(raw: UnsafeMutableRawPointer) {
-        self.init(raw.assumingMemoryBound(to: GModule.self))
+    /// - Parameter p: mutable raw pointer to the underlying object
+    public init(raw p: UnsafeMutableRawPointer) {
+        ptr = p
+    }
+
+    /// Unsafe untyped, retaining initialiser.
+    /// **Do not use unless you know the underlying data type the pointer points to conforms to `ModuleProtocol`.**
+    /// - Parameter raw: mutable raw pointer to the underlying object
+    public init(retainingRaw raw: UnsafeMutableRawPointer) {
+        ptr = raw
+        // no reference counting for GModule, cannot ref(cast(_ptr))
     }
 
     /// Unsafe untyped initialiser.
     /// **Do not use unless you know the underlying data type the pointer points to conforms to `ModuleProtocol`.**
-    public convenience init(opaquePointer: OpaquePointer) {
-        self.init(UnsafeMutablePointer<GModule>(opaquePointer))
+    /// - Parameter p: opaque pointer to the underlying object
+    public init(opaquePointer p: OpaquePointer) {
+        ptr = UnsafeMutableRawPointer(p)
+    }
+
+    /// Unsafe untyped, retaining initialiser.
+    /// **Do not use unless you know the underlying data type the pointer points to conforms to `ModuleProtocol`.**
+    /// - Parameter p: opaque pointer to the underlying object
+    public init(retainingOpaquePointer p: OpaquePointer) {
+        ptr = UnsafeMutableRawPointer(p)
+        // no reference counting for GModule, cannot ref(cast(_ptr))
     }
 
 
@@ -296,7 +343,7 @@ open class Module: ModuleProtocol {
     /// archive) it tries to open the corresponding module. If that fails
     /// and it doesn't have the proper module suffix for the platform
     /// (`G_MODULE_SUFFIX`), this suffix will be appended and the corresponding
-    /// module will be opended. If that fails and `file_name` doesn't have the
+    /// module will be opened. If that fails and `file_name` doesn't have the
     /// ".la"-suffix, this suffix is appended and `g_module_open()` tries to open
     /// the corresponding module. If eventually that fails as well, `nil` is
     /// returned.

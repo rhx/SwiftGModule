59a8089849355e97d8a5e641abfa5e00cb58a2bc
Rene Hexel <r.hexel@griffith.edu.au>
2020-08-31 07:23:45 +1000

    Note Swift 5.2 requirement

--- Package.swift
+++ Package.swift
@@ -1,15 +1,12 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.2
 
 import PackageDescription
 
 let package = Package(
     name: "GModule",
-    products: [
-        .library(name: "GModule", targets: ["GModule"]),
-    ],
+    products: [ .library(name: "GModule", targets: ["GModule"]) ],
     dependencies: [
-        .package(url: "ssh://promac.local/Users/rh/src/swift/rh/gtk/SwiftGLib", .branch("master"))
-        //.package(url: "https://github.com/rhx/SwiftGLib.git", .branch("master"))
+        .package(name: "GLib", url: "https://github.com/rhx/SwiftGLib.git", .branch("master"))
     ],
     targets: [
         .target(name: "GModule", dependencies: ["GLib"]),

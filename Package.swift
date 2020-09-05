def69914bf909c90ba401021271c08d7e6ae9cf3
Rene Hexel <r.hexel@griffith.edu.au>
2020-09-05 11:14:01 +1000

    merge master

--- Package.swift
+++ Package.swift
@@ -1,14 +1,12 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.2
 
 import PackageDescription
 
 let package = Package(
     name: "GModule",
-    products: [
-        .library(name: "GModule", targets: ["GModule"]),
-    ],
+    products: [ .library(name: "GModule", targets: ["GModule"]) ],
     dependencies: [
-        .package(url: "https://github.com/rhx/SwiftGLib.git", .branch("master"))
+        .package(name: "GLib", url: "https://github.com/rhx/SwiftGLib.git", .branch("master"))
     ],
     targets: [
         .target(name: "GModule", dependencies: ["GLib"]),

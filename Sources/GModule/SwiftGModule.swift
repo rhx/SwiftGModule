7df2c724f33f53a89fae8888872a45838edbee30
Rene Hexel <r.hexel@griffith.edu.au>
2020-09-05 11:31:57 +1000

    regen

---
+++ Sources/GModule/SwiftGModule.swift
@@ -0,0 +1,15 @@
+import CGLib
+import GLib
+
+public typealias _GModule_ModuleProtocol = ModuleProtocol
+
+
+public typealias _GModule_Module = Module
+
+public extension GModule {
+    typealias ModuleProtocol = _GModule_ModuleProtocol
+
+    typealias Module = _GModule_Module
+    typealias ModuleCheckInit = GModuleCheckInit
+    typealias ModuleUnload = GModuleUnload
+}

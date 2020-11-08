736e806edb1236af2131f925e7f0ba86e57720bd
Mikoláš Stuchlík <stuchlej@gmail.com>
2020-11-08 20:11:09 +0100

    add gir2swift

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ let package = Package(
     name: "GModule",
     products: [ .library(name: "GModule", targets: ["GModule"]) ],
     dependencies: [
+        .package(name: "gir2swift", url: "https://github.com/mikolasstuchlik/gir2swift.git", .branch("master")),
         .package(name: "GLib", url: "https://github.com/mikolasstuchlik/SwiftGLib.git", .branch("master"))
     ],
     targets: [
